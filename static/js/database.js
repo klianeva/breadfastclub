253b78fc00c5f13b82ffbeceac489a612b032ed7
Viktoria Klianeva <vikiklianeva@gmail.com>
2018-10-08 10:05:14 +0200

    update

--- static/js/database.js
+++ static/js/database.js
@@ -43,7 +43,6 @@ db.collection("products").get().then(function(querySnapshot) {
         });
 
         $("#containers").append(`<hr id="${category}"><div  class="productcategory container-fluid ">
-            <div class="backgroundimage"> ${background}
 			<div class="row " >
 			    <div class="col-lg-12" >
 			        <div class="content  backgroundgradient">
